cd6ad870502a33d89daa37ec9f39bbcc042862e6
Daniel Estrada <daestradam@unal.edu.co>
2022-09-04 22:42:58 -0500

    Solucion

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -3,3 +3,15 @@ class Asignatura:
     def __init__(self, nombre, salon="remoto"):
         self._nombre = nombre
         self._salon = salon
+
+    #Creaciones de metodo set, get y __str__   
+    def setNombre(self, nombre):
+        self._nombre = nombre
+    
+    def getNombre(self):
+        return self._nombre
+
+    def __str__(self):
+        return self._nombre + " " + self._salon
+
+
--- classroom/grupo.py
+++ classroom/grupo.py
@@ -1,33 +1,34 @@
 
 
 from classroom.asignatura import Asignatura
-from classroom.asignatura import Asignatura
+#from classroom.asignatura import Asignatura
 
 
 class Grupo:
 
-    grado = None
+    grado = "Grado 12" #None
 
     def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes=None):
         self._grupo = grupo
-        self._asignaturas = asignaturas
+        if asignaturas is None: #self._asignaturas = asignaturas
+            self._asignaturas = []
         self.listadoAlumnos = estudiantes
 
-    def listadoAsignaturas(self, kwargs):
+    def listadoAsignaturas(self, **kwargs): #kwargs
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
     def agregarAlumno(self, alumno, lista=[]):
         if(lista is None):
             lista.append(alumno)
-            self.listadoAlumnos = self.listadoAlumnos + lista
+            self.listadoAlumnos = [alumno] #self.listadoAlumnos = self.listadoAlumnos + lista
         else:
-            self.listadoAlumnos = [alumno]
+            self.listadoAlumnos = lista + [alumno] #self.listadoAlumnos = [alumno] 
 
     @ classmethod
-    def asignarNombre(cls, nombre="Grado 10"):
+    def asignarNombre(cls, nombre="Grado 6"):
         cls.grado = nombre
-
+    """"
     @ classmethod
     def asignarNombre(cls, nombre="Grado 6"):
         cls.grado = nombre
@@ -35,3 +36,8 @@ class Grupo:
     @ classmethod
     def asignarNombre(cls, nombre="Grado 4"):
         cls.grado = nombre
+    """
+    #creacion del metodo __str__
+    def __str__(self):
+        return "Grupo de estudiantes: grupo predeterminado"
+
